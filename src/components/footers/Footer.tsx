dcc2c743f62099cf5bd8294853039f592b2de02f
Jude Jawod <Jawod50@gmail.com>
2023-07-31 03:14:55 +0800

    Update the assets directory

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -1,19 +1,21 @@
 import React from "react";import Image from "next/image";
 import "./Footer.css";
+import Facebook from "../../../public/images/logos/SM_Facebook.png";
+
 function NavBar() {
 	return (
 		<div className="footer">
 			<a href="https://www.facebook.com/" target="new">
-				<Image src={"./images/logos/SM_Facebook.png"} alt="Facebook" />
+				<Image width={38} height={38} src={Facebook} alt="Facebook" />
 			</a>
 			<a href="https://www.instagram.com/" target="new">
-				<Image src={"./images/logos/SM_Instagram.png"} alt="Instagram" />
+				<Image width={38} height={38} src={"/images/logos/SM_Instagram.png"} alt="Instagram" />
 			</a>
 			<a href="https://twitter.com/" target="new">
-				<Image src={"./images/logos/SM_Twitter.png"} alt="Twitter" />
+				<Image width={38} height={38} src={"/images/logos/SM_Twitter.png"} alt="Twitter" />
 			</a>
 			<a href="https://linkedin.com/" target="new">
-				<Image src={"./images/logos/SM_Linkedin.png"} alt="Linkedin" />
+				<Image width={38} height={38} src={"/images/logos/SM_Linkedin.png"} alt="Linkedin" />
 			</a>
 			<p>Copyright © 2020-2021 Jude Russel Jawod</p>
 		</div>
