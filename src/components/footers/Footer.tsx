d0072ad267d566a21a4eefdc798a1a88867e90db
Jude Jawod <Jawod50@gmail.com>
2023-07-31 03:19:15 +0800

    Update the assets directory

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -1,21 +1,24 @@
 import React from "react";import Image from "next/image";
 import "./Footer.css";
-import Facebook from "../../../public/images/logos/SM_Facebook.png";
+import SM_Facebook from "../../../public/images/logos/SM_Facebook.png";
+import SM_Instagram from "../../../public/images/logos/SM_Instagram.png";
+import SM_Twitter from "../../../public/images/logos/SM_Twitter.png";
+import SM_Linkedin from "../../../public/images/logos/SM_Linkedin.png";
 
 function NavBar() {
 	return (
 		<div className="footer">
 			<a href="https://www.facebook.com/" target="new">
-				<Image width={38} height={38} src={Facebook} alt="Facebook" />
+				<Image width={38} height={38} src={SM_Facebook} alt="Facebook" />
 			</a>
 			<a href="https://www.instagram.com/" target="new">
-				<Image width={38} height={38} src={"/images/logos/SM_Instagram.png"} alt="Instagram" />
+				<Image width={38} height={38} src={SM_Instagram} alt="Instagram" />
 			</a>
 			<a href="https://twitter.com/" target="new">
-				<Image width={38} height={38} src={"/images/logos/SM_Twitter.png"} alt="Twitter" />
+				<Image width={38} height={38} src={SM_Twitter} alt="Twitter" />
 			</a>
 			<a href="https://linkedin.com/" target="new">
-				<Image width={38} height={38} src={"/images/logos/SM_Linkedin.png"} alt="Linkedin" />
+				<Image width={38} height={38} src={SM_Linkedin} alt="Linkedin" />
 			</a>
 			<p>Copyright © 2020-2021 Jude Russel Jawod</p>
 		</div>
