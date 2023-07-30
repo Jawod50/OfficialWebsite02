d16305961dd0db5928140a74c025451dc3652196
Jude Jawod <Jawod50@gmail.com>
2023-07-31 01:34:33 +0800

    Update the assets directory

--- src/pages/pages/_slideShowManula1.tsx
+++ src/pages/pages/_slideShowManula1.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import "./slideShowManula.css";
-const nextIcon = "../images/logos/next.png";
-const previousIcon = "../images/logos/previous.png";
+const nextIcon = "./images/logos/next.png";
+const previousIcon = "./images/logos/previous.png";
 
 interface IProps {
 	title: any;
@@ -10,15 +10,79 @@ interface IProps {
 	slideWidth: any;
 	slideHeight: any;
 	bgColor: any;
-	slides101: string[];
+	slides101: any;
 }
 
 function App(props: IProps) {
-	const [currentSlide, setCurrentSlide] = useState(props?.slides101 ? props?.slides101?.length : "");
+	const [currentSlide, setCurrentSlide] = useState(1);
 
 	return (
-		<div>
-			<h1>Hello</h1>
+		<div
+			className="content-container"
+			style={{
+				width: `${props.slideWidth}px`,
+				border: `10px solid rgb(${props.bgColor}, ${props.bgColor}, ${props.bgColor})`,
+				backgroundColor: `rgb(${props.bgColor}, ${props.bgColor}, ${props.bgColor})`,
+			}}
+		>
+			<div className="slideshow" style={{ width: `${props.slideWidth}px` }}>
+				<div
+					className="next-container"
+					onClick={() => {
+						if (props.slides101 ? props.slides101.length : 1 > currentSlide) setCurrentSlide(currentSlide + 1);
+					}}
+				>
+					<Image className="next-icon disable-select" alt="next" src={nextIcon} />
+				</div>
+				<div
+					className="previous-container"
+					onClick={() => {
+						if (currentSlide > 1) setCurrentSlide(currentSlide - 1);
+					}}
+				>
+					<Image className="previous-icon disable-select" alt="previous" src={previousIcon} />
+				</div>
+				<div className="slides" style={{ width: `${100 * props.slides101 ? props.slides101.length : 1}%` }}>
+					<div
+						className="slide"
+						style={
+							currentSlide == 1
+								? { width: `${100 / props.slides101 ? props.slides101.length : 1}%` }
+								: { marginLeft: `${-(currentSlide - 1) * (100 / props.slides101 ? props.slides101.length : 1)}%` }
+						}
+					>
+						<Image alt="slide" src={props.slides101[0]} />
+					</div>
+					{props.slides101.map((item: any, index: number) => {
+						if (index == 0) return;
+						return (
+							<div
+								className="slide"
+								style={{ width: `${100 / props.slides101 ? props.slides101.length : 1}%` }}
+								key={index}
+							>
+								<Image alt="slide" src={item} />
+							</div>
+						);
+					})}
+				</div>
+			</div>
+
+			<div className="slider-option">
+				{props.slides101.map((item: any, index: number) => (
+					<label
+						className="slider-bar"
+						style={currentSlide - 1 == index ? { backgroundColor: "#ffffff" } : { backgroundColor: "transparent" }}
+						key={index}
+						onClick={() => setCurrentSlide(index + 1)}
+					></label>
+				))}
+			</div>
+
+			<div className="detail-container">
+				<h1 className="title">{props.title}</h1>
+				<p className="description">{props.description}</p>
+			</div>
 		</div>
 	);
 }
